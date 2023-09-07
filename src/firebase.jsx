ccfb2b1a6e66bb2590c26ffa7fc9206662805875
Julie Shapovalenko <jools.shapo@gmail.com>
2023-09-07 15:47:39 +0300

    update firebase config

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBNVZFgytEjdMvwOde8nxXCOC7qITp-zno",
-  authDomain: "instantchat-35d43.firebaseapp.com",
+  authDomain: "instant-chat-project-pink.vercel.app",
   projectId: "instantchat-35d43",
   storageBucket: "instantchat-35d43.appspot.com",
   messagingSenderId: "361874264885",
